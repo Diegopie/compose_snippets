2a71efc62f29f3c4a44f2d3f0926239bf8b09dcb
Diego <diego@udundi.com>
2025-02-19 15:23:39 -0700

    update react scripts

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
+
+console.log(process.env.PORT)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5173,
+    port: process.env.PORT || 5173,
   },
   plugins: [react()],
 })
